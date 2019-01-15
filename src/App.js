75709e708ce2b8d6a5882b4bf3226be4032c0e18
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-01-15 16:54:46 +0700

    Add function getRoutes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
+import stations from './stations.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.visitedStation = [];
+    this.result = {};
+  }
+
+  getRoutes = (srcLine, destLine) => {
+    Object.keys(stations).forEach((stationName) => {
+      const stationLines = Object.keys(stations[stationName]);
+      if (!this.visitedStation.includes(stationName)
+      && stationLines.includes(srcLine) && stationLines.includes(destLine)) {
+        this.visitedStation.push(stationName);
+      }
+    });
+  }
+
   render() {
+    this.visitedStation = ['Bartley', 'Aljunied'];
+    const srcLines = Object.keys(stations.Bartley);
+    const destLines = Object.keys(stations.Aljunied);
+    srcLines.forEach((srcLine) => {
+      destLines.forEach((destLine) => {
+        this.getRoutes(srcLine, destLine);
+      });
+    });
+    console.log(this.visitedStation);
     return (
       <div className={styles.app}>
         <header className={styles.appHeader}>
