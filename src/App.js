2fd642ab705cf6c8843eddfdecfa757ddc9446cf
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-01-17 01:10:48 +0700

    Implement logic for routing

--- src/App.js
+++ src/App.js
@@ -4,31 +4,51 @@ import styles from './App.module.scss';
 import stations from './stations.json';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.visitedStation = [];
+  getAdjStations = (srcLine) => {
+    const adjStations = [];
+    Object.keys(stations).forEach((name) => {
+      const stationLines = Object.keys(stations[name]);
+      if (!this.visitedStations.includes(name) && stationLines.length > 1
+        && stationLines.includes(srcLine) && !adjStations.includes(name)) {
+        this.visitedStations.push(name);
+        adjStations.push(name);
+      }
+    });
+    return adjStations;
   }
 
-  getRoutes = (srcLine, destLine) => {
-    Object.keys(stations).forEach((stationName) => {
-      const stationLines = Object.keys(stations[stationName]);
-      if (!this.visitedStation.includes(stationName)
-      && stationLines.includes(srcLine) && stationLines.includes(destLine)) {
-        this.visitedStation.push(stationName);
+  getRoutes = (srcLines, destLine) => {
+    srcLines.find((srcLine) => {
+      if (srcLine === destLine) {
+        this.routes.push([...this.currentRoute]);
+        return true;
       }
+      if (!this.visitedLines.includes(srcLine)) {
+        this.visitedLines.push(srcLine);
+        const adjStations = this.getAdjStations(srcLine);
+        if (adjStations.length > 0) {
+          adjStations.forEach((s) => {
+            this.currentRoute.push(s);
+            const nextSrcLines = Object.keys(stations[s]);
+            this.getRoutes(nextSrcLines, destLine);
+            this.currentRoute.pop();
+          });
+        }
+      }
+      return false;
     });
   }
 
   render() {
-    this.visitedStation = ['Bartley', 'Aljunied'];
-    const srcLines = Object.keys(stations.Bartley);
-    const destLines = Object.keys(stations.Aljunied);
-    srcLines.forEach((srcLine) => {
-      destLines.forEach((destLine) => {
-        this.getRoutes(srcLine, destLine);
-      });
+    this.routes = [];
+    const srcLines = Object.keys(stations.Admiralty);
+    Object.keys(stations['Boon Lay']).forEach((destLine) => {
+      this.currentRoute = ['Admiralty'];
+      this.visitedStations = ['Admiralty'];
+      this.visitedLines = [];
+      this.getRoutes(srcLines, destLine);
     });
-    console.log(this.visitedStation);
+    console.log(this.routes);
     return (
       <div className={styles.app}>
         <header className={styles.appHeader}>
