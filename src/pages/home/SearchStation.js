b6c94155e9e53d5ffe814493ce78c5aaef5f7748
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-01-20 15:36:11 +0700

    set autofocus for choosing origin station input

--- src/pages/home/SearchStation.js
+++ src/pages/home/SearchStation.js
@@ -10,6 +10,7 @@ export default function SearchStation({
   return (
     <div className={styles.wrapper}>
       <AutoComplete
+        autoFocus
         className={styles.input}
         dataSource={dataSrcStations}
         onSearch={searchOriginStation}
